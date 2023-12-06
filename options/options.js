2855e9422cd2c2575334fb3470c2858a4d362092
Crowdbotics <team@crowdbotics.com>
2023-12-06 18:25:38 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "brokenlove-apparel-44880",
+  url: "https://brokenlove-apparel-44880.botics.co",
+  api: "https://brokenlove-apparel-44880.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
